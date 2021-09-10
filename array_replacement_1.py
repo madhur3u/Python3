789a51c8d432ae43a1ec609a353b4ca3d53a0ca7
Madhur Upadhyay <89251393+madhur3u@users.noreply.github.com>
2021-09-10 12:56:00 +0530

    Update array_replacement_1.py

--- array_replacement_1.py
+++ array_replacement_1.py
@@ -9,20 +9,20 @@ Input: (10,100,93,32,35,65,80,90,94,6) Output: (32,-1,94,35,65,80,90,94,-1,-1)''
 a = list(map(int, input().split()))
 l = len(a)
 x = -1
-b=[]
 i = 0
 max2 = max(a[i:l])
 while i < l - 1 :
     max2 = max(a[i:l])
     temp = max2
-
+    
     for j in range(i+1,l):
         if a[j] > a[i] and a[j] < max2 : max2 = a[j]
 
     
-    if max2 == temp and max2 <= a[i] : b.append(x)
-    else : b.append(max2)
+    if max2 == temp and max2 <= a[i] : a[i] = x
+    else : a[i] = max2
     i += 1
 
-b.append(x)
-print(b)
+a[i] = x
+print(a)
+    
