7ae68b7a87ae1fb2ba920fe5ece2bed36beb1981
Madhur Upadhyay <89251393+madhur3u@users.noreply.github.com>
2021-09-21 19:01:13 +0530

    Update Triplets_Min_Product.py

--- Triplets_Min_Product.py
+++ Triplets_Min_Product.py
@@ -1,10 +1,10 @@
 # Given an array of Int find min product among all triplets combinations
 
-a = list(map(int, input().split()))
-a.sort()
-p1 = a[0]*a[1]*a[2]
-p2 = a[0]*a[-1]*a[-2]
-print(min(p1,p2))
+# a = list(map(int, input().split()))
+# a.sort()
+# p1 = a[0]*a[1]*a[2]
+# p2 = a[0]*a[-1]*a[-2]
+# print(min(p1,p2))
 
 # O(n) approach without using sort            https://www.techiedelight.com/find-minimum-product-triplets-array/
 
