172e8961a201f04cfbfd51b4b8ef3e27b36e0f2a
Madhur Upadhyay <89251393+madhur3u@users.noreply.github.com>
2021-10-19 10:23:05 +0530

    Update NamedTuple.py

--- HKR/NamedTuple.py
+++ HKR/NamedTuple.py
@@ -1,18 +1,16 @@
-# https://www.hackerrank.com/challenges/py-collections-namedtuple/problem
-
 from collections import namedtuple
 
 n = int(input())
 
-fields = input()  # ID MARKS NAME CLASS --> this will be in fields
+fields = input().strip().split()        # ID MARKS NAME CLASS --> this will be in field
 student = namedtuple('student', fields) # make a named tuple of fields
 
 marks = 0   
 
 for _ in range(n):
     
-    i1, i2, i3, i4 = input().split()    # input fields value for each student
-    s1 = student(i1, i2, i3, i4)        # place it in named tuple now can be called using fields
-    marks += int(s1.MARKS)              # add the marks
+    i1, i2, i3, i4 = input().strip().split()    # input fields value for each student
+    s1 = student(i1, i2, i3, i4)                # place it in named tuple now can be called using fields
+    marks += int(s1.MARKS)                      # add the marks
     
 print(marks/n)  # average
